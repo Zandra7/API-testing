212aabf0651886bc2a8680adf5c8b9c50459f6a5
Sandra Eng <saena002@osloskolen.no>
2023-09-19 10:20:39 +0200

    Fixed undefined

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
 let out = ""
 let ut = document.getElementById("out")
-let checkmark
+let randJoke
 
 function klikk(){
  fetch("https://v2.jokeapi.dev/joke/Any")
-	 .then(respons => {
-		 return respons.json()
-	 })
-	 .then(data => {
-		 checkmark = data.joke
-		 out += checkmark
-         ut.innerHTML = out
-	 })
+	.then(respons => {
+		return respons.json()
+	})
+	.then(data => {
+		if(data.type == "single"){
+			randJoke = data.joke
+		} else {
+			randJoke = data.setup + "<br>" + "<br>" + data.delivery
+		}
+		out += randJoke
+        ut.innerHTML = out
+	})
 	
  out = ""
 }
